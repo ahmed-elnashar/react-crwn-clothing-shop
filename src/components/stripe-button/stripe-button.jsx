3e7518e6e106325641b89a416baafafe2d69b48d
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-06-02 05:57:44 +0200

    updating stripe intergration and updating our title

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey = 'pk_test_Z0mLmyFk0aRIjGzOYrvVVt1q001QkbYzaW';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      shippingAddress
+      billingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+
+export default StripeCheckoutButton;
